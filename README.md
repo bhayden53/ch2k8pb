5368b7ba4f633732a43deffb08ec2bde75cbedb9
bhayden53 <5826711+bhayden53@users.noreply.github.com>
2019-12-10 18:40:24 -0500

    initial commit for OperationSports

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ch2k8pb
 Random Custom Playbooks for CH2k8
+
+## Setup ##
+
+#### plays.txt ####
+This file contains the definition of which plays are to be assigned for which position, and for which types of players. i.e. "sg wing iso" is a play for a SG who you've listed as being suitable for an iso or playmaker role (good handles, speed, passing, offensive awareness, layup, etc.)
+
+#### plays.config ####
+The main configuration file. You can setup the playbook to be weighted towards whichever positions you want by using this file. For each "scorer" block, define the position that the block is for, which "skills" the player is good at, and how many plays you want the playbook to have for that position. The total number of plays in a custom playbook is 20.
+
+#### the skills ####
+__iso__ - handles, speed, layup, etc. 
+__post__ - inside, low O, Shoots in Traffic
+__pnr__ - pick and roll ball handler
+__3pt__ - 3 point rating
+__mr__ - mid range rating
+__inside__ - players that you want to have opportunities at the rim, either via being the roll player in the pick and roll, or a cutter.
+__drive__ - for players that you want to drive, but not necessarily iso
+__playmaker__ - there are some plays where a player receives the ball and has 2-3 passing options for open shots. This player should have good O AWR and Passing. 
+
+#### dependencies ####
+python 3
+numpy
+astropy
+
+#### final notes ####
+I threw this together in an evening. There's a good chance there's a bug or two where the script will fail for some combinations of scorers and skills. 
+
+
---
+++ playbook.py
@@ -0,0 +1,60 @@
+import configparser
+from astropy.table import Table
+import numpy as np
+
+config = configparser.ConfigParser()
+config.read('./plays.config')
+
+plays = Table.read('./plays.txt', format='ascii')
+
+sections = config.sections()
+
+play_types = ["man", "both", "zone"]
+
+playbook = []
+for s in sections:
+    if "scorer" in s:
+        pos = config[s]["pos"]
+        skills = eval(config[s]["skills"])
+        num = int(config[s]["num"])
+        ind = np.where(plays['pos'] == pos)
+        tmp = plays[ind]
+
+        num_assigned = 0
+        for skill in skills:
+            skill_ind = np.char.find(tmp['type'], skill) == 0
+            skill_tmp = tmp[skill_ind]
+
+            if len(skill_tmp) == 0:
+                continue
+            selection = np.random.randint(len(skill_tmp))
+            for_player = (skill_tmp['playbook'][selection], skill_tmp['play'][selection])
+            if for_player not in playbook:
+                playbook.append(for_player)
+                # print(num_assigned, pos, for_player, skill)
+                num_assigned += 1
+
+        # print(num_assigned, num, skills)
+        # print(playbook)
+        assert(num_assigned <= num)
+        while num_assigned < num:
+            for pt in play_types:
+                if num_assigned >= num:
+                    continue
+                ind2 = np.where(tmp['defense'] == pt)
+                tmp2 = tmp[ind2]
+                if len(tmp2) == 0:
+                    continue
+
+
+                selection = np.random.randint(len(tmp2))
+                player_types = tmp2['type'][selection].split(' ')
+                for pt2 in player_types:
+                    if pt2 in skills:
+                        for_player = (tmp2['playbook'][selection], tmp2['play'][selection])
+                        if for_player not in playbook:
+                            playbook.append(for_player)
+                            num_assigned += 1
+
+for p in playbook:
+    print(p)
